8881cbf7f60730be5ca00adf0d1244f749a486cb
thatericchen <echen7266@gmail.com>
2025-09-06 14:11:03 -0400

    Initial Commit

---
+++ MoodBubble/MoodBubbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodBubbleApp.swift
+//  MoodBubble
+//
+//  Created by Eric Chen on 9/4/25.
+//
+
+import SwiftUI
+
+@main
+struct MoodBubbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
